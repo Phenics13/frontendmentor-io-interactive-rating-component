0f2ee79bbac1d8ee8163f6a073d8871c3a5fb68b
Phenics13 <optron920@gmail.com>
2023-03-15 14:35:43 +0200

    added animation with framer motion

--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -6,11 +6,24 @@ import { CardContainer } from "./card.styles";
 
 import CardRate from "../card-rate/card-rate.component";
 import CardThanks from "../card-thanks/card-thanks.component";
+import { motion } from "framer-motion";
 
 function Card() {
   const { submit } = useContext(CurrentNumberContext);
+
   return (
-    <CardContainer>{submit ? <CardThanks /> : <CardRate />}</CardContainer>
+    <CardContainer
+      as={motion.div}
+      initial={{ rotate: 180, scale: 0 }}
+      animate={{ rotate: 0, scale: 1 }}
+      transition={{
+        type: "spring",
+        stiffness: 260,
+        damping: 30,
+      }}
+    >
+      {submit ? <CardThanks /> : <CardRate />}
+    </CardContainer>
   );
 }
 
